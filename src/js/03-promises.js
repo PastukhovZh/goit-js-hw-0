a64eacca5eee9e7f0659eaf13ad9caf4d98be3e9
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-07-28 01:25:10 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import Notiflix, { Notify } from 'notiflix';
+
+
+const form = document.querySelector('.form');
+let delayValue;
+let stepValue;
+let amountValue;
+
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
