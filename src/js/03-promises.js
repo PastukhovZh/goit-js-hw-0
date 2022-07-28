22a99f5b7bff127759fe1e579416904dd08cf440
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-07-28 23:50:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,20 +2,51 @@ import Notiflix, { Notify } from 'notiflix';
 
 
 const form = document.querySelector('.form');
-let delayValue;
-let stepValue;
-let amountValue;
 
-
+form.addEventListener('submit', onSubmit);
 
 
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+    
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay})
   } else {
-    // Reject
+    reject({position, delay})
+  } 
+    }, delay)
+    
+  })
+  
+}
+
+
+
+function onSubmit(e) {
+  e.preventDefault();
+
+
+  
+  const { elements: { delay, step, amount } } = e.currentTarget;
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i++) {
+createPromise(i, delayValue)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });    
+    delayValue += stepValue
+
+    e.currentTarget.reset();
+
   }
 }
